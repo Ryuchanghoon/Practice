29fe538e7e60b78208788699ed4df69396c3d432
류창훈 <fbckdgns3@naver.com>
2023-03-28 13:41:37 +0900

    [fix] 백준 1302번 수정.

--- BaeckJoon/1302.py
+++ BaeckJoon/1302.py
@@ -3,15 +3,22 @@ from collections import Counter
 
 input = sys.stdin.readline
 
-book_list = []
+n = int(input())
+books = [input() for _ in range(n)]
 
-for i in range(int(input())):
-    book_list.append(input())
+count_books = Counter(books) #위에 받아준  books 빈도수
 
-count_list = Counter(book_list)
+max_count = max(count_books.values()) #빈도수 가장 큰 것, 받아와서 딕셔너리 값만.
 
-max_count = count_list.most_common(n = 1)
+# 출판횟수 가장 많은 책들. 리스트로
+best_sellers = []
+for book, count in count_books.items():
+    if count == max_count:
+        best_sellers.append(book)
 
-print(max(count_list))
+# 사전순으로 가장 앞에 나오는 제목.
+best_seller = sorted(best_sellers)[0]
 
-########### 실패 문제.
+print(best_seller)
+
+## 사전순으로 앞서는 것을 고려 안했다.
