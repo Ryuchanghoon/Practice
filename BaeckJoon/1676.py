2926ef42a480b7bf97c84d2dcb588a39b6915b1c
류창훈 <fbckdgns3@naver.com>
2023-05-04 22:51:04 +0900

    [upload] 백준 1676번 파이썬 문제풀이.

--- BaeckJoon/1676.py
+++ BaeckJoon/1676.py
@@ -1,25 +1,12 @@
-#소인수 분해 계산. 2 & 5로 나누어 떨어지는 거 계산.
-
 import sys
-
 input = sys.stdin.readline
 
-
 n = int(input())
+count = 0
 
-count_a = 0
-count_b = 0
+# 5 배수마다 0 한개씩
+while n >= 5:
+    count += n // 5
+    n //= 5
 
-
-for i in range(1, n+1):
-    
-    while i % 2 == 0: #2로 나누어 떨어지는 경우.
-        count_a += 1 # 나누어 떨어질 때 마다, 1 증가 시키고,
-        i = i // 2 # i값 2로 나누고
-
-    while i % 5 == 0: # 5로 나누어 떨어지는 경우.
-        count_b += 1 # 동일하게 5 나누어 떨어지는 경우, 1 증가.
-        i = i // 2 # i값 끝날때까지 계속 2 나누기.
-
-
-print(min(count_a, count_b))
+print(count)
